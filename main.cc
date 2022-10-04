2594e1e58a7761585a73a4912ae509ddf9e3639c
unknown <krishnasubedi2366@gmail.com>
2022-10-04 08:10:18 +0545

    test

--- main.cc
+++ main.cc
@@ -1,26 +1,5 @@
 #include <iostream>
 
-class Distance
-{
-
-private:
-    int feet;
-    float inches;
-
-public:
-    Distance()
-    {
-        feet = 0;
-        inches = 0.0;
-    }
-
-    Distance(int i, float t)
-    {
-        feet = i;
-        inches = t;
-    }
-};
-
 int main()
 {
     std::cout << "The best way to do ti \n";
